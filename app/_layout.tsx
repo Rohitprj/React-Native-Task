951d0759f13f27fb26e3e5959f0b25613a449a3e
Rohit kumar <rohitprj.1282@gmail.com>
2025-06-18 11:58:11 +0530

    feat: Add Bookings screen with filtering and booking creation functionality
    - Implemented Bookings screen with filters for store, status, paid status, customer type, and date.
    - Added functionality to create new direct bookings and existing bookings.
    - Integrated API calls for fetching bookings, customers, stores, and packages.
    - Enhanced UI with modals for booking creation and improved loading/error handling.
    - Updated package.json to include @react-navigation/drawer for navigation.
    - Created layout for admin drawer with tab navigation for the Bookings screen.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,10 +41,56 @@ export default function RootLayout() {
       ) : userRole === "SUBADMIN" ? (
         <Stack.Screen name="(subAdmin)" />
       ) : userRole === "EMPLOYEE" ? (
-        <Stack.Screen name="(employee)" />
+        <Stack.Screen name="(adminDrawer)" />
       ) : (
         ""
       )}
     </Stack>
   );
 }
+
+// import { getUserData } from "@/utils/tokenStorage";
+// import { useRouter } from "expo-router";
+// import { useEffect, useState } from "react";
+// import { ActivityIndicator, View } from "react-native";
+
+// export default function RootLayout() {
+//   const router = useRouter();
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const check = async () => {
+//       const userData = await getUserData();
+//       console.log("PersistData", userData);
+
+//       if (userData?.role) {
+//         const role = userData.role.toUpperCase();
+//         if (role === "ADMIN") {
+//           router.replace("/(tabs)");
+//         } else if (role === "SUBADMIN") {
+//           router.replace("/(subAdmin)");
+//         } else if (role === "EMPLOYEE") {
+//           router.replace("/(employee)/Bookings");
+//         } else {
+//           router.replace("/");
+//         }
+//       } else {
+//         router.replace("/");
+//       }
+
+//       setLoading(false);
+//     };
+
+//     check();
+//   }, []);
+
+//   if (loading) {
+//     return (
+//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//         <ActivityIndicator size="large" />
+//       </View>
+//     );
+//   }
+
+//   return null; // Or fallback screen briefly shown during redirect
+// }
